447a0707dd9efe0c4360f29ba822235b3b193e8f
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-03 22:34:17 +0530

    Add auth middleware.

---
+++ middleware/auth.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+
+// Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // Set token from authorization header
+    token = req.headers.authorization.split(' ')[1];
+  }
+  // Set token from cookie
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
+
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse('Unauthorized access', 401));
+  }
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (err) {
+    next(new ErrorResponse('Unauthorized access', 401));
+  }
+});
